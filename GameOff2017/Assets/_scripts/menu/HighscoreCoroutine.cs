a51750b0cba48c31c5bf2ffff7a1f9104ba0d6bd
Andrew Radford <andrewradford125@gmail.com>
2017-11-12 13:23:03 -0700

    Start Menu

---
+++ GameOff2017/Assets/_scripts/menu/HighscoreCoroutine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreCoroutine : MonoBehaviour {
+
+    public List<GameObject> scores;
+
+	void Start () {
+        StartCoroutine(Highscores());
+	}
+	void Update () {
+		
+	}
+
+    IEnumerator Highscores()
+    {
+        for (int i = 0; i < 5; i++){
+            yield return new WaitForSeconds(1);
+            scores[i].gameObject.SetActive(true);
+        }
+    }
+}
