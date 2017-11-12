0dd26c1cc8d5cd6236cb31b34144facccaa10940
Andrew Radford <andrewradford125@gmail.com>
2017-11-12 13:47:59 -0700

    Start Import

---
+++ GameOff2017/Assets/_scripts/menu/ImageImport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageImport : MonoBehaviour {
+
+    public string url = "https://mitten25.github.io/img/DumpedIcon.PNG";
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }
+}
