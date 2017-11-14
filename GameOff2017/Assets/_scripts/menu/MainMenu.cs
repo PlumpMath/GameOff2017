61e2f6e2ca13b7f69d0cc0e20d788fc1065907d0
Andrew Radford <andrewradford125@gmail.com>
2017-11-13 17:14:35 -0700

    Start meta manager

---
+++ GameOff2017/Assets/_scripts/menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    //input manager
+    private Player input_manager;
+
+    // Use this for initialization
+    void Start () {
+
+        //initialize input manage
+        input_manager = ReInput.players.GetPlayer(0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Insert coin to continue
+		if (input_manager.GetButtonDown("jump")){
+            SceneManager.LoadScene("level");
+        }
+	}
+}
