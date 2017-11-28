ed9b67f30b85a499f7d0f8f7c4384dfd7c207991
Andrew Radford <andrewradford125@gmail.com>
2017-11-27 18:27:23 -0700

    Highscore

--- GameOff2017/Assets/_scripts/managers/ScoreManager.cs
+++ GameOff2017/Assets/_scripts/managers/ScoreManager.cs
@@ -5,16 +5,102 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
+    public static ScoreManager instance;
+
     public Text oneup_text;
     public Text highscore_text;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    const string privateCode = "Jvzp8VG8SESuQeP4na6_2wWz65cAmS0k - U2akXaNaWkA";
+    const string publicCode = "5a1c6d3e6b2b65d418be08f6";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public Highscore[] highscoresList;
+
+    void Awake()
+    {
+        //singleton setup
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //AddNewHighscore("Sebastian", 50);
+        //AddNewHighscore("Mary", 85);
+        //AddNewHighscore("Bob", 92);
+
+        DownloadHighscores();
+    }
+
+    void Start()
+    {
+        //set highscore
+        //new WaitForSeconds(1);
+        //print(highscoresList);
+        //print(highscore_text);
+        //highscore_text.text = highscoresList[0].score.ToString("000000");
+    }
+
+    public void AddNewHighscore(string username, int score)
+    {
+        StartCoroutine(UploadNewHighscore(username, score));
+    }
+
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+            print("Upload Successful");
+        else
+        {
+            print("Error uploading: " + www.error);
+        }
+    }
+
+    public void DownloadHighscores()
+    {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+
+    IEnumerator DownloadHighscoresFromDatabase()
+    {
+        WWW www = new WWW(webURL + publicCode + "/pipe/0/5");
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+            FormatHighscores(www.text);
+        else
+        {
+            print("Error Downloading: " + www.error);
+        }
+    }
+
+    void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoresList[i] = new Highscore(username, score);
+            print(highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+    }
+
+}
+
+public struct Highscore
+{
+    public string username;
+    public int score;
+
+    public Highscore(string _username, int _score)
+    {
+        username = _username;
+        score = _score;
+    }
 }
--- GameOff2017/Assets/_scripts/menu/HighscoreCoroutine.cs
+++ GameOff2017/Assets/_scripts/menu/HighscoreCoroutine.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighscoreCoroutine : MonoBehaviour {
 
     public List<GameObject> scores;
 
-	void Start () {
+    private List<string> places;
+
+    private void Awake()
+    {
+        ScoreManager.instance.DownloadHighscores();
+    }
+
+    void Start ()
+    {
         StartCoroutine(Highscores());
+        places = new List<string>() { "1st", "2nd", "3rd", "4th", "5th" };
 	}
 	void Update () {
 		
@@ -17,6 +28,9 @@ public class HighscoreCoroutine : MonoBehaviour {
     {
         for (int i = 0; i < 5; i++){
             yield return new WaitForSeconds(1);
+            ScoreManager.instance.highscore_text.text = ScoreManager.instance.highscoresList[0].score.ToString("000000");
+            Regex reg = new Regex("([A-Z])+");
+            scores[i].gameObject.GetComponent<Text>().text = places[i] + "     " + ScoreManager.instance.highscoresList[i].score.ToString("000000          ") + reg.Match(ScoreManager.instance.highscoresList[i].username);
             scores[i].gameObject.SetActive(true);
         }
     }
