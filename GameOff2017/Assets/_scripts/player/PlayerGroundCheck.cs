e46bf21db79ab335190ef4628dbc7860b33ec1cf
Andrew Taylor Johnson <andrewtaylorjohnson@gmail.com>
2017-12-01 10:32:03 -0700

    fixed grounded bug

--- GameOff2017/Assets/_scripts/player/PlayerGroundCheck.cs
+++ GameOff2017/Assets/_scripts/player/PlayerGroundCheck.cs
@@ -9,32 +9,36 @@ public class PlayerGroundCheck : MonoBehaviour
     private void Start()
     {
         //get player height
-        player_height = PlayerController.instance.sprite.bounds.size.y;
+        player_height = PlayerController.instance.GetComponent<BoxCollider2D>().bounds.size.y;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        Debug.Log(player_height);
+        Debug.Log(PlayerController.instance.transform.position.y + 0.35f);
+        Debug.Log(collision.gameObject.transform.position.y +
+                    (collision.gameObject.GetComponent<BoxCollider2D>().bounds.size.y / 2));
         if (collision.gameObject.CompareTag("ground"))
         {
-            //if (PlayerController.instance.transform.position.y - (player_height / 2) > collision.gameObject.transform.position.y +
-            //        (collision.gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2))
+            if (PlayerController.instance.transform.position.y + 0.35f > collision.gameObject.transform.position.y +
+                    (collision.gameObject.GetComponent<BoxCollider2D>().bounds.size.y / 2))
                 PlayerController.instance.grounded = true;
-            //else
-            //    PlayerController.instance.grounded = false;
+            else
+                PlayerController.instance.grounded = false;
 
         }
         else if(collision.gameObject.CompareTag("pizza"))
         {
-            if (PlayerController.instance.transform.position.y - (player_height / 2) > collision.gameObject.transform.position.y +
-                    (PizzaController.instance.sprite.bounds.size.y / 2))
+            if (PlayerController.instance.transform.position.y + 0.35f > collision.gameObject.transform.position.y +
+                    (PizzaController.instance.GetComponent<BoxCollider2D>().bounds.size.y / 2))
                 PlayerController.instance.grounded = true;
             else
                 PlayerController.instance.grounded = false;
         }
         else if(collision.gameObject.CompareTag("end_level") && !PlayerController.instance.beat_level)
         {
-            //if (PlayerController.instance.transform.position.y - (player_height / 2) > collision.gameObject.transform.position.y +
-            //        (collision.gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2))
+            if (PlayerController.instance.transform.position.y + 0.35f > collision.gameObject.transform.position.y +
+                    (collision.gameObject.GetComponent<BoxCollider2D>().bounds.size.y / 2))
                 PlayerController.instance.StartCoroutine(PlayerController.instance.LevelComplete());
         }
     }
